fee8de7224c46f4704403774d7a6261929249c17
JasperGwyn <jgwyn@protonmail.com>
2025-03-28 12:09:20 -0300

    Simplify configuration to match local dev environment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  trailingSlash: true,
   env: {
     X_API_KEY: process.env.X_API_KEY,
     X_API_SECRET: process.env.X_API_SECRET,
